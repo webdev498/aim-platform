d73b3be1543005d690afd875645c20c3d1ab63d9
webdev498 <gcmobile@mail.com>
2016-12-01 08:48:56 -0600

    run swagger.yaml as localhost api

---
+++ packages/swagger-server/controllers/Default.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var url = require('url');
+
+
+var Default = require('./DefaultService');
+
+
+module.exports.dataDataIdGET = function dataDataIdGET (req, res, next) {
+  Default.dataDataIdGET(req.swagger.params, res, next);
+};
+
+module.exports.dataFindByTypeTypeidGET = function dataFindByTypeTypeidGET (req, res, next) {
+  Default.dataFindByTypeTypeidGET(req.swagger.params, res, next);
+};
+
+module.exports.dataGET = function dataGET (req, res, next) {
+  Default.dataGET(req.swagger.params, res, next);
+};
+
+module.exports.dataTypesDataTypeIdGET = function dataTypesDataTypeIdGET (req, res, next) {
+  Default.dataTypesDataTypeIdGET(req.swagger.params, res, next);
+};
+
+module.exports.dataTypesFindByPlatformPlatformIdGET = function dataTypesFindByPlatformPlatformIdGET (req, res, next) {
+  Default.dataTypesFindByPlatformPlatformIdGET(req.swagger.params, res, next);
+};
+
+module.exports.dataTypesGET = function dataTypesGET (req, res, next) {
+  Default.dataTypesGET(req.swagger.params, res, next);
+};
+
+module.exports.groupsGET = function groupsGET (req, res, next) {
+  Default.groupsGET(req.swagger.params, res, next);
+};
+
+module.exports.groupsGroupIdGET = function groupsGroupIdGET (req, res, next) {
+  Default.groupsGroupIdGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsFindByDomainGET = function platformsFindByDomainGET (req, res, next) {
+  Default.platformsFindByDomainGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsGET = function platformsGET (req, res, next) {
+  Default.platformsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdApprovalsGET = function platformsPlatformIdApprovalsGET (req, res, next) {
+  Default.platformsPlatformIdApprovalsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdBoatsGET = function platformsPlatformIdBoatsGET (req, res, next) {
+  Default.platformsPlatformIdBoatsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdClientsGET = function platformsPlatformIdClientsGET (req, res, next) {
+  Default.platformsPlatformIdClientsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdDashboardGET = function platformsPlatformIdDashboardGET (req, res, next) {
+  Default.platformsPlatformIdDashboardGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdDfaGET = function platformsPlatformIdDfaGET (req, res, next) {
+  Default.platformsPlatformIdDfaGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdEventsGET = function platformsPlatformIdEventsGET (req, res, next) {
+  Default.platformsPlatformIdEventsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdFormsGET = function platformsPlatformIdFormsGET (req, res, next) {
+  Default.platformsPlatformIdFormsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdGET = function platformsPlatformIdGET (req, res, next) {
+  Default.platformsPlatformIdGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdMapsGET = function platformsPlatformIdMapsGET (req, res, next) {
+  Default.platformsPlatformIdMapsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdMediaGET = function platformsPlatformIdMediaGET (req, res, next) {
+  Default.platformsPlatformIdMediaGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdModulesGET = function platformsPlatformIdModulesGET (req, res, next) {
+  Default.platformsPlatformIdModulesGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdModulesModuleIdGET = function platformsPlatformIdModulesModuleIdGET (req, res, next) {
+  Default.platformsPlatformIdModulesModuleIdGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdNewsGET = function platformsPlatformIdNewsGET (req, res, next) {
+  Default.platformsPlatformIdNewsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdPagesGET = function platformsPlatformIdPagesGET (req, res, next) {
+  Default.platformsPlatformIdPagesGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdProductsGET = function platformsPlatformIdProductsGET (req, res, next) {
+  Default.platformsPlatformIdProductsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdReportsGET = function platformsPlatformIdReportsGET (req, res, next) {
+  Default.platformsPlatformIdReportsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdShowsGET = function platformsPlatformIdShowsGET (req, res, next) {
+  Default.platformsPlatformIdShowsGET(req.swagger.params, res, next);
+};
+
+module.exports.platformsPlatformIdSitesGET = function platformsPlatformIdSitesGET (req, res, next) {
+  Default.platformsPlatformIdSitesGET(req.swagger.params, res, next);
+};
+
+module.exports.rootGET = function rootGET (req, res, next) {
+  Default.rootGET(req.swagger.params, res, next);
+};
+
+module.exports.usersFindByEmailGET = function usersFindByEmailGET (req, res, next) {
+  Default.usersFindByEmailGET(req.swagger.params, res, next);
+};
+
+module.exports.usersGET = function usersGET (req, res, next) {
+  Default.usersGET(req.swagger.params, res, next);
+};
+
+module.exports.usersUserIdProfileGET = function usersUserIdProfileGET (req, res, next) {
+  Default.usersUserIdProfileGET(req.swagger.params, res, next);
+};
